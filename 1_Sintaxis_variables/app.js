9f0f0300e0c688b6b652a9b12d8bbe67d69ab802
juancl2007 <lmjcamilo@gmail.com>
2025-07-31 12:26:14 -0500

    repositorio js versión 1

---
+++ 1_Sintaxis_variables/app.js
@@ -0,0 +1,23 @@
+//alert ("HOLA MUNDO"); 
+//prompt ("dime tu nombre")
+
+//variable declarada con let 
+let personaje = "Goku";
+ 
+//crear variable puntaje y asignar un valor a 0
+let puntaje = 0;
+// crear variable de shots y asignar un valor de 3
+let shots = 3;
+    recarga = 2;
+
+//console.log("TOTAL DE BALAS:", shots + recarga);
+
+//el ususario capturp 4 patos
+puntaje = puntaje + 4;
+//el usuario gasto 2 balas
+shots = shots - 2;
+//el usuario recargo su arma
+recarga += 1;
+console.log("Puntaje:", puntaje);
+console.log("Balas restantes:", shots);
+console.log("Balas recargadas:", recarga);
---
+++ 2_operadores_comparacion_logicos/app2.js
@@ -0,0 +1,33 @@
+//bolean(..)
+
+console.log( ' "5" == "5" ', "5" == "5" ); // true
+console.log( ' "5" == "5" ', "5" === "5" ); // true
+console.log( ' "5" == "5" ', 5 === "5" ); // false
+console.log( ' "5" == "5" ', "5" == "5" ); // true
+console.log( ' "5" == "5" ', "5" === (3+2) ); // false
+console.log( ' "5" == "5" ', 5 === 5 ); // true
+
+// Calculadora sencilla
+let operacion = prompt("¿Qué operación quieres hacer? (sumar, restar, multiplicar, dividir)");
+let num1 = parseFloat(prompt("Ingresa el primer número:"));
+let num2 = parseFloat(prompt("Ingresa el segundo número:"));
+let resultado;
+
+if (operacion === "sumar") {
+   resultado = num1 + num2;
+} else if (operacion === "restar") {
+  resultado = num1 - num2;
+} else if (operacion === "multiplicar") {
+    resultado = num1 * num2;
+} else if (operacion === "dividir") {
+    if (num2 !== 0) {
+        resultado = num1 / num2;
+    } else {
+        resultado = "Error: No se puede dividir por cero.";
+    }
+} else {
+  resultado = "Operación no válida.";
+}
+
+alert("El resultado es: " + resultado);
+
